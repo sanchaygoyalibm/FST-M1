cc365c09b963c1330243ff5e4565a89aca4dd3c7
sanchaygoyalibm <143474582+sanchaygoyalibm@users.noreply.github.com>
2023-12-11 13:02:10 +0530

    Add files via upload

---
+++ Selenium/Activities/Activity1.java
@@ -0,0 +1,16 @@
+package activities;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Activity1 {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.training-support.net");
+        driver.close();
+    }
+
+}
+
